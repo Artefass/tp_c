5da9ac0773fcec1efdd0d534403c1d5f4a423ae0
Artefass <kpa0art.university@gmail.com>
2019-03-02 02:02:42 +0300

    разработал прототип модуля для извлечения токенов. компиляцию и тесты не делал

--- lab2/token.c
+++ lab2/token.c
@@ -1,19 +1,35 @@
 #include "token.h"
 
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
 #define PTRN_STRING "^\"\d\""
 #define PTRN_NUMBER "^\d"
 #define PTRN_OPERATION "^(*|+|-|(|))"
 
-#define SET_PATTERN_TOKEN(type, name, pattern) (token_t){.type=type,.name=name,.pattern=pattern}
+typedef struct regex_token_t {
+    token_type type;
+    char *name;
+    char *pattern;
+    regex_t re;
+} regex_token_t;
+
+#define SET_PATTERN_TOKEN(type, name, pattern) (regex_token_t){(type), (name), (pattern)}
 
 static regex_token_t regex_tokens[] = {
-    SET_PATTERN_TOKEN(STRING,'string', PTRN_STRING),
-    SET_PATTERN_TOKEN(NUMBER,'number', PTRN_NUMBER),
-    SET_PATTERN_TOKEN(OPERATION,'operation',PTRN_OPERATION)    
-}
+    SET_PATTERN_TOKEN(STRING,"string", PTRN_STRING),
+    SET_PATTERN_TOKEN(NUMBER,"number", PTRN_NUMBER),
+    SET_PATTERN_TOKEN(OPERATION,"operation",PTRN_OPERATION)    
+};
+
+static bool init = false;
 
 static int regex_init()
 {
+    assert(!init);
+
     int error;
     for (int i = 0; i < ARRAYNUM(regex_tokens); i++)
     {
@@ -23,24 +39,95 @@ static int regex_init()
             return error;
         }
     }
-
+    init = true;
     return 0;
 }
 
-
-static int match_token(const char *expression, token_t token) 
+static void regex_free()
 {
-    assert(expression != NULL);
+    assert(init);
 
-    token_t token = {.type=UNNOWN};
+    for (int i = 0; i < ARRAYNUM(regex_tokens); i++)
+    {
+         regfree(&regex_tokens[i].re);
+    }
+}
 
-    regmatch_t match_data;
-    for (int i = 0; i < ARRAYNUM(regex_tokens); i++){
-        if (!regexec(&regex_tokens[i].re, expression, match_data, )){
-            
-        } 
+static token_t* new_token(){
+    token_t *token = calloc(sizeof(token_t), 1);
+    
+    if (!token) 
+    {
+        printf("Error: new_token faild. could not allocate memmory\n");
+        return NULL;
     }
 
+    token->type = UNKNOWN;
     return token;
 }
 
+static void free_token(token_t *token)
+{
+    assert(token);
+
+    if (token->name) free(token->name);
+    if (token->value) free(token->value);
+
+    free(token);
+}
+
+static bool init_token(const regex_token_t *regex_token, token_t *token, const char *str, int length)
+{
+    token->name = strdup(regex_token->name);
+    token->value = strndup(str, length);
+    token->type = regex_token->type;
+    if (!token->name || !token->type)
+    {
+        if (token->name) free(token->name);
+        if (token->value) free(token->value);
+
+        printf("Error: init_token faild. could not allocate memmory\n");   
+        return false;         
+    }
+
+    return true;
+}
+
+static bool match_token(const char *str, token_t *token) 
+{
+    assert(str != NULL && token != NULL);
+
+    regmatch_t match_data;
+    for (int i = 0; i < ARRAYNUM(regex_tokens); i++)
+    {
+        if (!regexec(&regex_tokens[i].re, str, 1, &match_data, 0))
+        {
+            int len = match_data.rm_eo - match_data.rm_so;
+            if (init_token(&regex_tokens[i], token, str, len)){
+                str += len;
+                return true;
+            }
+        } 
+    }
+
+    return false;
+}
+
+token_t *get_token(const char *str)
+{
+    // пропуск пробелов
+    while(*str == ' ' && *str != '\0') str++;
+
+    token_t *token;
+    if (!(token = new_token())) 
+    {
+        return NULL;
+    }
+
+    if (!match_token(str, token)){
+        free_token(&token);
+        return NULL;
+    }
+    
+    return token;
+}
--- lab2/token.h
+++ lab2/token.h
@@ -1,50 +1,19 @@
 #pragma once
 
 #include <regex.h>
+#include <stdbool.h>
 
 typedef enum {
-    UNNOWN,
+    UNKNOWN,
     STRING,
-    INT,
-    OPERATON
+    NUMBER,
+    OPERATION
 } token_type;
 
 typedef struct token_t {
-    token_type type,
+    token_type type;
     char *name;
     char *value;
 } token_t;
 
-typedef struct regex_token_t {
-    token_type type,
-    char *name,
-    char *pattern;
-    regex_t re,
-} regex_token_t;
-
-
-
-
-
-
-
-
-
-token_t get_token(const char *expression){
-    // пропуск пробелов
-    while(*expression == ' ' and *expression != '\0')
-        expression++;
-
-    if *exp
-    
-    token_t *match_token = NULL;
-    if (match_operand_string(expression, match_token) {}
-        return match_token;
-    }
-
-    if (match_operation(expression, match_token)) {
-        token_t token = {.type.OPERATON, .token=match_token};
-        return token;
-    } 
-}
-
+token_t *get_token(const char *str);
